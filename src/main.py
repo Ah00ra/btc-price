ba110143f51018c54b722bed36e7368ba4842ffe
Ah00ra <naghipour.ahoora@gmail.com>
2021-11-11 22:43:20 +0330

    Use sqlite3 instead of mysql

--- src/main.py
+++ src/main.py
@@ -1,31 +1,26 @@
 import datetime
-import json
-import mysql.connector
+import sqlite3
 import requests as req
 
-### get BTC price with API and write in database ###
-
+### get BTC price with API and write it in the database ###
 def bitcoin_price():
     url = "https://api.coindesk.com/v1/bpi/currentprice.json"
     r = req.get(url)
-    btc_price = r.json()['bpi']['USD']['rate_float']
+    btc_price = r.json()["bpi"]["USD"]["rate_float"]
     return int(btc_price)
 
 
-def w_mysql(price, date):
-    cnx = mysql.connector.connect(user='root', password='root', database='test')
-
-    cursor = cnx.cursor()
-
-    cursor.execute(f"INSERT INTO btc VALUES('{price}', '{date}')")
-    cnx.commit()
-
-    cursor.close()
-    cnx.close()
+def w_data(price, date):
+    """write in databse"""
+    conn = sqlite3.connect("data.db")
+    cursor = conn.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS btc (prices INTEGER, date DATE)""")
+    cursor.execute(f"INSERT INTO btc VALUES({price}, '{date}')")
+    conn.commit()
+    conn.close()
 
 
 price = bitcoin_price()
 date = str(datetime.date.today())
 
-w_mysql(price, date)
-
+w_data(price, date)
