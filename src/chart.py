868f78ed35916238559883aa737a8b7fceb31651
Ah00ra <naghipour.ahoora@gmail.com>
2021-11-11 22:44:09 +0330

    Read data from sqlite3 instead of mysql

--- src/chart.py
+++ src/chart.py
@@ -1,34 +1,33 @@
 import matplotlib.pyplot as plt
-import mysql.connector
+import sys
+import sqlite3
 
-# read data from the database and save a chart as JEPG
-cnx = mysql.connector.connect(user='root', password='root', database='test')
+db_path = sys.argv[1]
+conn = sqlite3.connect(db_path)
+cursor = conn.cursor()
 
-query = ("SELECT * FROM btc")
-
-cursor = cnx.cursor()
+query = "SELECT * FROM btc"
 cursor.execute(query)
+record = cursor.fetchall()
+conn.close()
 
 prices = []
 dates = []
+for price, date in record:
+    prices.append(price)
+    dates.append(date)
 
-for price, date in cursor:
-    prices.append(price), dates.append(date)
-
-cnx.close()
-cursor.close()
-
-# create chart and insert data.
-plt.title('BTC price chart')
+# # create chart and insert data.
+plt.title("BTC price chart")
 plt.xlabel("date")
-plt.ylabel('prices')
+plt.ylabel("prices")
 
 plt.grid(True)
-plt.plot(dates, prices, 'bo--', linewidth=2, markersize=10)
+plt.plot(dates, prices, "bo--", linewidth=2, markersize=10)
 
 fig = plt.gcf()
+# TODO; dynamic figure size instead of fix one
 fig.set_size_inches((19, 10))
 
 # Increasing the DPI helps you get a large figure, except format like PNG which doesn't know about inches.
-fig.savefig('./btc-chart.jpg', bbox_inches='tight')
-
+fig.savefig("./btc-chart.jpg", bbox_inches="tight")
